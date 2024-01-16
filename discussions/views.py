abcf4e607861fa1427b8e009f6710f2628c3bf53
Ariel Carmen <arielahognisse@gmail.com>
2024-01-16 19:23:01 +0100

    discussion view exemples added

--- discussions/views.py
+++ discussions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import discussions_collection
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>App running...<h1>")
+
+def add(request):
+    records = {
+        "u1" : "u1",
+        "u2" : "u2"
+    }
+    discussions_collection.insert_one(records)
+    return HttpResponse("<h1>User added<h1>")
+
+def get_all(request):
+    discussions = discussions_collection.find()
+    return discussions
