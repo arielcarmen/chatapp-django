3f2d2ab33330b6b9d7e6bd03a8c0d1086a0a8430
Ariel Carmen <arielahognisse@gmail.com>
2024-01-16 19:31:41 +0100

    mongo connexion is ok

--- discussions/models.py
+++ discussions/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 from db_connexion import db
 
-# Create your models here.
-
 discussions_collection = db['discussions']
--- discussions/urls.py
+++ discussions/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', view= views.index),
-    path('/add', view= views.add),
-    path('/all', view= views.get_all),
+    path('add/', view= views.add),
+    path('all/', view= views.get_all),
 ]
