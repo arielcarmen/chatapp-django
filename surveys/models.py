b87465142560499af2240cde25f33c055fbab6d1
Ariel Carmen <arielahognisse@gmail.com>
2024-01-17 22:42:11 +0100

    surveys app configured

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+from db_connexion import db
+
+surveys_collection = db['surveys']
--- surveys/views.py
+++ surveys/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import surveys_collection
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>App isrunning...<h1>")
+
+def add(request):
+    records = {
+        "u1" : "u1",
+        "u2" : "u2"
+    }
+    surveys_collection.insert_one(records)
+    return HttpResponse("<h1>User added<h1>")
+
+def get_all(request):
+    surveys = surveys_collection.find()
+    return surveys
