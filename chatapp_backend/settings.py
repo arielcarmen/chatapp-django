2ea631ca2653eac32f4e99bd11c4e10a77bd9edf
Ariel Carmen <arielahognisse@gmail.com>
2024-02-15 09:56:18 +0100

    message creation...

--- chatapp_backend/settings.py
+++ chatapp_backend/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'discussions'
+    'discussions',
+    'messages',
+    'surveys',
+    'contacts',
+    'users'
 ]
 
 
--- messages/models.py
+++ messages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from db_connexion import db
+import time
 
 messages_collection = db['messages']
 
@@ -8,13 +9,22 @@ class DBMessageManager:
     def __init__(self):
         self.collection = messages_collection
 
-    def create_contact(self, user2Id):
+    def create_message(self, user2Id):
+        filename = ""
+        filetype = ""
+        filepath = ""
+        filesize = ""
+
         contact_data = {
             "user1Id": "65cd2c9425f598db480f1639",
-            "user2Id": user2Id,
-            "status": "PENDING",
-            "user1Blocked": False,
-            "user2Blocked": False,  
-            "added_at" : time.time()
+            "text": user2Id,
+            "responseToMsgId": "PENDING",
+            "created_at" : time.time(),
+            "file" : {
+                "name": filename,
+                "type": filetype,
+                "pathUrl": filepath,
+                "size": filesize
+            }
         }
         return self.collection.insert_one(messages_collection)
